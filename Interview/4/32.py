f478bf3dbebf00cac308e1979c7a47781e34925a
Administrator <2635681517@qq.com>
2021-03-08 19:15:02 +0800

    获取昨天日期

---
+++ Interview/4/32.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/3/8 0008
+# @Author : yang
+# @Email : 2635681517@qq.com
+# @File : 32.py
+"""Python list 常用操作"""
+# 1、list的创建
+list1 = ["a", "b", "mpilgrim", "z", "example"]
+print(list1)
+print(list1[1])
+print(list1[:3])
+print(list1[1:4])
+"""包含左边不包含右边正索引从0开始，负索引从-1开始"""
+# 2、list 负数索引
+print(list1[-3])
+# 3.list 增加元素
+# 4.list 搜索
+# 5.list 删除元素
+# 6.list 运算符
+# 7.使用join链接list成为字符串
+# 8.list 分割字符串
+# 9.list 的映射解析
+# 10.dictionary 中的解析
+# 11.list 过滤
+
+if __name__ == "__main__":
+    pass
