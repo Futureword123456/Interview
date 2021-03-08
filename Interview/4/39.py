dad6d1c79ae60aa39c87a96b36ea53a8dbae103e
Administrator <2635681517@qq.com>
2021-03-08 21:23:55 +0800

    Python 翻转列表

---
+++ Interview/4/39.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/3/8 0008
+# @Author : yang
+# @Email : 2635681517@qq.com
+# @File : 39.py
+"""Python 翻转列表"""
+l = [1,2,5,9,8]
+l.reverse()
+print(l)
+
+if __name__ == "__main__":
+    pass
