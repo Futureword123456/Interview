5ade2c9eb85222e7530427d1af817979d48988ad
Administrator <2635681517@qq.com>
2021-03-08 21:36:59 +0800

    翻转列表

--- Interview/4/39.py
+++ Interview/4/39.py
@@ -4,9 +4,18 @@
 # @Email : 2635681517@qq.com
 # @File : 39.py
 """Python 翻转列表"""
-l = [1,2,5,9,8]
+l = [1, 2, 5, 9, 8]
+print(l[::-1])
 l.reverse()
 print(l)
 
+
+# print(l)
+def reverse(list1):
+    for i in range(1, len(list1) + 1):
+        return list1[i+3::-1]
+
+
 if __name__ == "__main__":
-    pass
+    l = [1, 2, 5, 9, 8]
+    print(reverse(l))
